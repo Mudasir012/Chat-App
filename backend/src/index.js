ed29fc5d80a1bf8c66bc7053190d1cf925bcac96
mudasir <mallahmudasir07@gmail.com>
2026-05-09 01:44:23 +0500

    ok

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,7 +34,6 @@ app.use("/api/calls", callRoutes);
 app.post("/api/pusher/auth", protectRoute, (req, res) => {
   const socketId = req.body.socket_id;
   const channel = req.body.channel_name;
-  
   const presenceData = {
     user_id: req.user._id.toString(),
     user_info: {
